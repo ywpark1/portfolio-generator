4758754f16f89166a5a481429a593110f15bf219
Yuecheng Wu <ywu194@myseneca.ca>
2018-11-05 15:54:27 -0500

    minor fix

--- Portfolio-generator-console/Program.cs
+++ Portfolio-generator-console/Program.cs
@@ -5,7 +5,7 @@ namespace Portfolio_generator_console {
     class Program {
         static void Main (string[] args) {
             HtmlGenerator.SelectTemplate ();
-            HtmlGenerator.generateHtml ();
+            HtmlGenerator.GenerateHtml ();
         }
     }
 }
