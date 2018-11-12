1fa1f0fa6823f5ae179e8f4c82f7b77e6e132c10
Yeonwoo Park <ywoo1234@gmail.com>
2018-11-12 17:14:47 -0500

    Add DeleteFiles function, clean up the code,  add comments on each function

--- Portfolio-generator-console/Program.cs
+++ Portfolio-generator-console/Program.cs
@@ -4,7 +4,6 @@ namespace Portfolio_generator_console {
 
     class Program {
         static void Main (string[] args) {
-            HtmlGenerator.SelectTemplate ();
             HtmlGenerator.GenerateHtml ();
         }
     }
